b263101c05601b2058af2df62af3655ba83987b4
John Mota <johnmarcos.mota@gmail.com>
2024-01-16 19:40:40 -0300

    excepitions

--- src/main/java/br/com/johnmota/rest/excepitions/ExcepitionsResponse.java
+++ src/main/java/br/com/johnmota/rest/excepitions/ExcepitionsResponse.java
@@ -1,6 +1,31 @@
 package br.com.johnmota.rest.excepitions;
 
-public class ExcepitionsResponse {
+import java.io.Serializable;
+import java.util.Date;
 
-    
+public class ExcepitionsResponse implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    private Date timestamp;
+    private String message;
+    private String details;
+
+    public ExcepitionsResponse(Date timestamp, String message, String details) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
 }
