8d04b3d03aeab10be28091ba6360f5c7e564db20
John Mota <johnmarcos.mota@gmail.com>
2024-01-15 21:17:26 -0300

    iniciando o projeto

---
+++ src/test/java/br/com/johnmota/rest/RestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.johnmota.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
