e428b72294909bba76d31872e1343f7762f6342a
John Mota <johnmarcos.mota@gmail.com>
2024-03-13 09:30:17 -0300

    created database

--- src/main/java/br/com/johnmota/rest/models/Person.java
+++ src/main/java/br/com/johnmota/rest/models/Person.java
@@ -2,15 +2,36 @@ package br.com.johnmota.rest.models;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "persons")
 public class Person implements Serializable {
   
   private static final long serialVersionUID = 1L;
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @Column(name = "first_name", nullable = false, length = 150)
   private String firstName;
+
+  @Column(name = "last_name", nullable = false, length = 150)
   private String lastName;
+
+  @Column(nullable = false, length = 100)
   private String addres;
+
+  @Column(nullable = false)
   private String gener;
+
+  @Column(nullable = false, length = 9) 
   private Integer age;
 
   public Person () {}
