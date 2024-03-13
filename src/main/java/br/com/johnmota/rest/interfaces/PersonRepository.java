33fa9b62b00913fb7f855d2df5d25c45390baab9
John Mota <johnmarcos.mota@gmail.com>
2024-03-13 20:20:45 -0300

    melhorias

--- src/main/java/br/com/johnmota/rest/interfaces/PersonRepository.java
+++ src/main/java/br/com/johnmota/rest/interfaces/PersonRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import br.com.johnmota.rest.models.Person;
 
-@Repository
+
 public interface PersonRepository extends JpaRepository<Person, Long> {}
 
 
